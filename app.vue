71817b930f22499b6dd178c966868751e1fe7e29
Alois Sečkár <ellrohir@seznam.cz>
2024-02-26 21:50:21 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="p-4 h-screen flex flex-col items-center justify-center bg-slate-900 text-[#ff6c43]">
+    <h1 class="mb-4 text-4xl font-bold">
+      PragVue
+    </h1>
+    <div class="mb-2 text-2xl">
+      Vue.js konference
+    </div>
+    <div class="mb-2 text-2xl font-bold">
+      16. září 2024
+    </div>
+    <div>
+      MORE TO COME
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'PragVue 2024',
+  htmlAttrs: {
+    lang: 'cs'
+  }
+})
+</script>
