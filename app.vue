663d8c4497d5da5a80861e1d62b0b2da2cbfa00f
Alois Sečkár <ellrohir@seznam.cz>
2024-02-29 21:07:00 +0100

    fix: hydration mismatch

--- app.vue
+++ app.vue
@@ -2,13 +2,15 @@
   <div class="h-screen flex flex-col items-center justify-center bg-[url('/bg-light.jpg')] dark:bg-[url('/bg-dark.jpg')] bg-top bg-cover opacity-95 p-2">
     <div class="w-full sm:w-4/5 lg:w-1/2 m-2 mt-20 p-8 text-center bg-slate-900 opacity-90 text-[#ff6c43] border border-black rounded-xl">
       <div>
-        <UButton
-          :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
-          color="gray"
-          variant="ghost"
-          aria-label="Theme"
-          @click="isDark = !isDark"
-        />
+        <ClientOnly>
+          <UButton
+            :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+            color="gray"
+            variant="ghost"
+            aria-label="Theme"
+            @click="isDark = !isDark"
+          />
+        </ClientOnly>
         <h1 class="mb-4 text-4xl font-bold">
           PragVue
         </h1>
@@ -27,6 +29,8 @@
 </template>
 
 <script setup lang="ts">
+import type { ClientOnly } from '#build/components'
+
 useHead({
   title: 'PragVue 2024',
   htmlAttrs: {
