8e1c19100eb4401c5d01d2c8b9abcb97110c30e3
Alois Sečkár <ellrohir@seznam.cz>
2024-03-07 18:59:44 +0100

    feat: drop light/dark theme

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="h-screen flex flex-col items-center justify-center bg-[url('/bg-light.jpg')] dark:bg-[url('/bg-dark.jpg')] bg-top bg-cover opacity-95 p-2">
+  <div class="h-screen flex flex-col items-center justify-center bg-[url('/bg-dark.jpg')] bg-top bg-cover opacity-95 p-2">
     <div class="w-full sm:w-4/5 lg:w-1/2 m-2 absolute bottom-8 p-8 text-center bg-slate-900 opacity-90 text-[#ff6c43] border border-black rounded-xl">
       <div>
-        <img class="w-32 h-32 mx-auto cursor-pointer opacity-100" src="/logo.webp" @click="isDark = !isDark">
+        <img class="w-32 h-32 mx-auto cursor-pointer opacity-100" src="/logo.webp">
         <h1 class="mb-4 text-4xl font-bold">
           PragVue
         </h1>
@@ -25,16 +25,6 @@ useHead({
   title: 'PragVue 2024',
   htmlAttrs: {
     lang: 'cs'
-  }
-})
-
-const colorMode = useColorMode()
-const isDark = computed({
-  get () {
-    return colorMode.value === 'dark'
-  },
-  set () {
-    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   }
 })
 </script>
