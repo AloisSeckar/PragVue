ed109b8b8f5a3c298f77be738a682458856fa6d4
Alois Sečkár <ellrohir@seznam.cz>
2024-03-07 19:22:35 +0100

    feat: logo and some style

--- app.vue
+++ app.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="h-screen flex flex-col items-center justify-center bg-[url('/bg-dark.jpg')] bg-top bg-cover opacity-95 p-2">
-    <div class="w-full sm:w-4/5 lg:w-1/2 m-2 absolute bottom-8 p-8 text-center bg-slate-900 opacity-90 text-[#ff6c43] border border-black rounded-xl">
-      <div>
-        <img class="w-32 h-32 mx-auto cursor-pointer opacity-100" src="/logo.webp">
-        <h1 class="mb-4 text-4xl font-bold">
-          PragVue
+    <div class="w-full sm:w-5/6 lg:w-2/3 m-2 absolute bottom-8 p-4 text-center bg-slate-900 opacity-90 text-[#ff6c43] border border-black rounded-xl">
+      <div class="flex flex-col sm:flex-row justify-center mb-4">
+        <img class="inline-block mx-auto sm:mx-0 w-32 h-32 cursor-pointer opacity-100" src="/logo2.webp">
+        <h1 class="inline-block ml-4 text-[80px] font-bold">
+          PragVue 2024
         </h1>
-        <div class="mb-2 text-2xl">
-          Vue.js konference
-        </div>
-        <div class="mb-2 text-2xl font-bold">
-          Září 2024
-        </div>
-        <div>
-          MORE TO COME
-        </div>
+      </div>
+      <div class="mb-2 text-2xl">
+        Konference o Vue.js
+      </div>
+      <div class="mb-2 text-2xl font-bold">
+        Září 2024
+      </div>
+      <div>
+        MORE TO COME
       </div>
     </div>
   </div>
