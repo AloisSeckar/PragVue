c01c2fe95985d42a9a58884924f59ab119949e66
Alois Sečkár <ellrohir@seznam.cz>
2024-04-06 22:25:42 +0200

    feat: prague background redesign

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="h-screen flex flex-col items-center justify-center bg-[url('/bg-dark.jpg')] bg-top bg-cover opacity-95 p-2">
-    <div class="w-[calc(100%-16px)] md:w-5/6 lg:w-2/3 m-2 absolute bottom-8 px-4 py-6 text-center bg-slate-900 opacity-90 text-[#ff6c43] border border-black rounded-xl">
-      <TheLanguageSelector />
+  <div class="flex flex-col h-screen items-center justify-center bg-[#3c3c3c]">
+    <div class="w-full max-w-[800px] h-[256px] cursor-pointer bg-white bg-[url('/prague.webp')] bg-center bg-cover" />
+    <div class="w-full max-w-[800px] flex-grow bg-[#1a1a1a]" />
+    <div class="w-full max-w-[800px] px-8 py-6 text-center text-[#ff6c43] absolute bottom-12">
+      <!-- <TheLanguageSelector /> -->
       <a href="https://digitalnispolecnost.cz/">
         <img
           class="inline-block mx-auto my-4 w-[200px] cursor-pointer"
@@ -13,14 +15,14 @@
       </div>
       <div class="flex flex-col min-[660px]:flex-row justify-center mb-4">
         <img
-          class="inline-block mx-auto min-[660px]:mx-0 w-28 h-28 cursor-pointer opacity-100"
+          class="hidden min-[660px]:inline-block mx-auto min-[660px]:mx-0 w-28 h-28 cursor-pointer opacity-100"
           src="/logo-vue.webp"
         >
         <h1 class="inline-block mx-4 text-[72px] font-bold">
           PragVue 2024
         </h1>
         <img
-          class="inline-block mx-auto min-[660px]:mx-0 w-28 h-28 cursor-pointer opacity-100"
+          class="hidden min-[660px]:inline-block mx-auto min-[660px]:mx-0 w-28 h-28 cursor-pointer opacity-100"
           src="/logo-prg.webp"
         >
       </div>
