01162b3110d4c1fb79be7ee4b70aa54a2a37a8bf
AloisSeckar <ellrohir@seznam.cz>
2024-04-08 15:40:38 +0200

    feat: landing page logos

--- components/TheIntro.vue
+++ components/TheIntro.vue
@@ -2,37 +2,36 @@
   <div class="flex flex-col h-screen items-center justify-center bg-[#3c3c3c]">
     <div class="w-full max-w-[800px] h-[256px] cursor-pointer bg-white bg-[url('/prague.webp')] bg-center bg-cover" />
     <div class="w-full max-w-[800px] flex-grow bg-[#1a1a1a]" />
-    <div class="w-full max-w-[800px] px-8 py-6 text-center text-[#ff6c43] absolute bottom-12">
+    <div class="w-full max-w-[800px] px-8 py-6 text-center text-[#ff6c43] absolute bottom-8 sm:bottom-32">
       <TheLanguageSelector />
-      <a href="https://digitalnispolecnost.cz/">
-        <img
-          class="inline-block mx-auto my-4 w-[200px] cursor-pointer"
-          src="/eviden.webp"
-        >
-      </a>
-      <div class="text-xl">
+      <div class="flex flex-col sm:flex-row gap-4 justify-center items-center text-white font-bold">
+        <a href="https://vuejs.org/">
+          <img class="inline-block mx-auto my-4 w-auto h-[40px] cursor-pointer" src="/vue.png">
+        </a>
+        <span class="hidden sm:inline">&</span>
+        <a href="https://digitalnispolecnost.cz/">
+          <img class="inline-block mx-auto my-4 w-auto h-[40px] cursor-pointer" src="/eviden.webp">
+        </a>
+        <span class="hidden sm:inline">&</span>
+        <a href="https://www.exponet.cz/">
+          <img class="inline-block mx-auto my-4 w-auto h-[40px] cursor-pointer" src="/exponet.png">
+        </a>
+      </div>
+      <div class="text-xl text-white">
         {{ $t('info.presents') }}
       </div>
       <div class="flex flex-col min-[660px]:flex-row justify-center mb-4">
-        <img
-          class="hidden min-[660px]:inline-block mx-auto min-[660px]:mx-0 w-28 h-28 cursor-pointer opacity-100"
-          src="/logo-vue.webp"
-        >
         <h1 class="inline-block mx-4 text-[72px] font-bold">
           PragVue 2024
         </h1>
-        <img
-          class="hidden min-[660px]:inline-block mx-auto min-[660px]:mx-0 w-28 h-28 cursor-pointer opacity-100"
-          src="/logo-prg.webp"
-        >
       </div>
-      <div class="mb-1 text-2xl">
+      <div class="mb-1 text-2xl text-white">
         {{ $t('info.about') }}
       </div>
       <div class="mb-1 text-2xl font-bold">
         {{ $t('info.date') }}
       </div>
-      <div class="mb-6 text-xl">
+      <div class="mb-6 text-xl text-white">
         {{ $t('info.place') }}
         <a href="https://maps.app.goo.gl/BksoN2qFxf61Tn2v8">🗺️</a>
       </div>
