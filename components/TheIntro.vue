1c57a8ca799d80e35561b4468d7848640435e3d1
AloisSeckar <ellrohir@seznam.cz>
2024-04-09 09:54:29 +0200

    fix: adjust logos

--- components/TheIntro.vue
+++ components/TheIntro.vue
@@ -5,11 +5,11 @@
       <TheLanguageSelector />
       <div class="flex flex-col sm:flex-row gap-4 my-4 justify-center items-center text-[#dddddd] font-bold">
         <a href="https://vuejs.org/">
-          <img class="inline-block w-auto h-7 sm:h-10 cursor-pointer" src="/vue.png" alt="Vue.js logo">
+          <img class="inline-block w-auto h-14 sm:h-16 cursor-pointer" src="/vue.png" alt="Vue.js logo">
         </a>
         <span class="hidden sm:inline">&</span>
         <a href="https://digitalnispolecnost.cz/">
-          <img class="inline-block w-auto h-7 sm:h-10 cursor-pointer" src="/eviden.webp" alt="Eviden logo">
+          <img class="inline-block w-auto h-7 sm:h-8 cursor-pointer" src="/eviden.webp" alt="Eviden logo">
         </a>
         <span class="hidden sm:inline">&</span>
         <a href="https://www.exponet.cz/">
