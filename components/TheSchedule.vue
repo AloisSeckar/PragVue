7a49d07f30a1fa0b3a53eb489618dbcc8180da0b
AloisSeckar <ellrohir@seznam.cz>
2024-04-09 09:36:10 +0200

    feat: schedule

--- components/TheSchedule.vue
+++ components/TheSchedule.vue
@@ -1,7 +1,113 @@
 <template>
   <div class="flex flex-col h-screen px-4 items-center justify-start  bg-[#3c3c3c]">
     <a name="schedule"><h2 class="mt-4 mb-4 text-4xl text-[#ff6c43] font-bold">{{ $t('schedule.title') }}</h2></a>
-    <p>Koho uvidíte a uslyšíte (program)</p>
+    <p>
+      {{ $t('intro.date') }}
+      {{ $t('intro.place') }}
+      <a href="https://maps.app.goo.gl/BksoN2qFxf61Tn2v8">🗺️</a>
+    </p>
+    <div class="flex flex-col lg:flex-row gap-0 lg:gap-4 my-2 text-xl">
+      <div class="flex flex-col gap-2">
+        <div class="flex gap-4">
+          <div class="font-bold">
+            09:00 - 09:15
+          </div>
+          <div>
+            Vue App in 15 mins
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            09:15 - 09:50
+          </div>
+          <div>
+            Keynote - what's new in Vue.js?
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            09:50 - 10:30
+          </div>
+          <div>
+            To the roots of Vue
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            10:30 - 10:50
+          </div>
+          <div>
+            1st Coffee Break 🥐☕
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            10:50 - 11:25
+          </div>
+          <div>
+            Meet Nuxt
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            11:25 - 12:00
+          </div>
+          <div>
+            Introduction to Vue ecosystem
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2">
+        <div class="flex gap-4">
+          <div class="font-bold">
+            12:00 - 13:00
+          </div>
+          <div>
+            Lunch Break 🍽️🥗
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            13:00 - 13:40
+          </div>
+          <div>
+            TBA
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            13:40 - 14:20
+          </div>
+          <div>
+            TBA
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            14:20 - 14:40
+          </div>
+          <div>
+            2nd Coffee Break 🥐☕
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            14:40 - 15:20
+          </div>
+          <div>
+            Case study - implementing Nuxt/Vue
+          </div>
+        </div>
+        <div class="flex gap-4">
+          <div class="font-bold">
+            15:20 - 16:00
+          </div>
+          <div>
+            TBA
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="flex mt-4 gap-2">
       <a href="https://www.konference.cz/" class="rounded-lg hover:border-2 hover:border-[#ff6c43]" :title="$t('schedule.join')">
         <img src="/nav-schedule-join.jpg" class="rounded-lg w-[250px] h-[120px]" alt="Join PragVue conference">
