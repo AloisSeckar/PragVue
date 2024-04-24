ce7d5f797ea094479475a7ff2d4e49ebb23e8c9e
AloisSeckar <ellrohir@seznam.cz>
2024-04-24 08:00:58 +0200

    feat: add basic meta tags

--- app.vue
+++ app.vue
@@ -10,10 +10,19 @@
 
 <script setup lang="ts">
 useHead({
-  title: 'PragVue 2024',
   htmlAttrs: {
     lang: 'en',
   },
+})
+useSeoMeta({
+  title: 'PragVue 2024',
+  description: 'The first Czech Vue.js conference in Prague (17th September 2024)',
+  ogType: 'website',
+  ogUrl: 'https://pragvue.com/',
+  ogTitle: 'PragVue 2024',
+  ogDescription: 'The first Czech Vue.js conference in Prague (17th September 2024)',
+  ogImage: 'https://pragvue.com/pragvue.jpg',
+  twitterCard: 'summary_large_image',
 })
 </script>
 
