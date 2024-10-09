d4b4b86d9bb90c381b876025f310530ea3440d7b
AloisSeckar <ellrohir@seznam.cz>
2024-10-09 21:48:59 +0200

    feat: return logos to landing page

--- components/TheIntro.vue
+++ components/TheIntro.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen items-center justify-center bg-[url('/bg1.webp')] bg-center bg-cover">
     <div class="w-full max-w-[800px] p-2 flex flex-col flex-grow justify-center text-center">
-      <!--
       <div class="flex flex-col md:flex-row gap-6 md:gap-2 my-4 justify-center items-center text-[#dddddd] font-bold">
         <a href="https://www.evidencz.com/">
           <img
             class="inline-block w-auto h-4 md:h-8 cursor-pointer" src="/eviden.webp"
             title="Eviden Czech Republic" alt="Eviden logo"
-          >
-        </a>
-        <span class="hidden md:inline">&</span>
-        <a href="https://www.gopas.cz/">
-          <img
-            class="inline-block w-auto h-6 md:h-10 cursor-pointer" src="/gopas.webp"
-            title="IT training center GOPAS" alt="Gopas logo"
           >
         </a>
         <span class="hidden md:inline">&</span>
@@ -23,21 +15,13 @@
             title="Exponet - Your Event Manager" alt="Exponet logo"
           >
         </a>
-        <span class="hidden md:inline">&</span>
-        <a href="https://www.startupjobs.cz/">
-          <img
-            class="inline-block w-auto h-6 md:h-12 md:pt-2 cursor-pointer" src="/startupjobs.webp"
-            title="StartupJobs.cz" alt="StartupJobs.cz logo"
-          >
-        </a>
       </div>
       <div class="text-xl text-[#dddddd] mb-6 md:mb-8">
         {{ $t('intro.present') }}
       </div>
-      -->
       <div>
         <img
-          class="inline-block w-auto h-40 cursor-pointer mb-2" src="/pragvue.svg"
+          class="inline-block w-auto h-32 cursor-pointer mb-2" src="/pragvue.svg"
           title="PragVue" alt="PragVue logo"
         >
       </div>
