64594630d2c8887b722485b0464c507b67cc1750
Matej Cerny <matej.cern@gmail.com>
2025-01-18 23:32:24 +0100

    chore: switch to Nuxt v4 breaking changes & update deps

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,10 +1,5 @@
-import en from '@/assets/en.json'
-
 export default defineI18nConfig(() => ({
   legacy: false,
-  locale: 'en',
-  fallbackLocale: 'en',
-  messages: { en },
   warnHtmlMessage: false,
   detectBrowserLanguage: {
     useCookie: false,
