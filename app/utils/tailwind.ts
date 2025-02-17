4ef54e4473cee4611977c2e877d56d64b909426d
AloisSeckar <ellrohir@seznam.cz>
2025-02-17 12:21:35 +0100

    refactor: remove old files

--- app/utils/tailwind.ts
+++
@@ -1,9 +0,0 @@
-export const PAGE_BLOCK = 'flex flex-col lg:flex-row gap-8 lg:gap-12 min-h-screen p-8 items-center justify-start bg-center bg-cover'
-
-export const CONTENT_LEFT = 'inline-block lg:w-1/2 text-center lg:text-right'
-export const CONTENT_RIGHT = 'inline-block lg:w-1/2 text-center lg:text-left'
-
-export const CARD_GRAY = 'px-6 py-4 bg-[#dddddd] text-black rounded-xl'
-
-export const BUTTON_LIGHT = 'inline-block mx-auto px-12 py-4 bg-[#dddddd] rounded-3xl text-2xl text-black font-bold hover:text-[#dddddd] hover:border-2 hover:border-[#dddddd] hover:bg-[#002D3C]'
-export const BUTTON_DARK = 'inline-block mx-auto px-10 py-4 bg-[#002D3C] rounded-3xl text-2xl text-white font-bold hover:text-black hover:border-2 hover:border-black hover:bg-white'
