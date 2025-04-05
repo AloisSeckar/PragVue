764190026f98554b8daa367f93e0ccf4910fa70b
p4w3l <93704910+czarsbenemoriendi@users.noreply.github.com>
2025-04-05 02:52:59 +0200

    refactor: split index page into sections

--- app/components/layout/ScheduleSection.vue
+++ app/components/layout/ScheduleSection.vue
@@ -11,3 +11,21 @@
     </LayoutContainerContent>
   </LayoutContainer>
 </template>
+
+<template>
+  <LayoutContainer>
+    <LayoutContainerContent>
+      <UiHeading class="mb-6">
+        The schedule
+      </UiHeading>
+    </LayoutContainerContent>
+  </LayoutContainer>
+
+  <LayoutContainer>
+    <LayoutContainerContent>
+      <UiHeading class="mb-6">
+        Get your ticket
+      </UiHeading>
+    </LayoutContainerContent>
+  </LayoutContainer>
+</template>
