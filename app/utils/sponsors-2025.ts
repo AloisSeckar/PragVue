2d88949220645859a3c1f0c2899375ac49cb7d28
Alois Sečkár <ellrohir@seznam.cz>
2025-04-14 22:59:33 +0200

    refactor: move speaker and sponsor info into separate data files

---
+++ app/utils/sponsors-2025.ts
@@ -0,0 +1,22 @@
+export const sponsors = [
+  [
+    {
+      to: 'https://www.evidencz.com/',
+      image: 'atos',
+      title: 'Atos Czech Republic',
+      alt: 'Atos logo',
+    },
+    {
+      to: 'https://www.evidencz.com/',
+      image: 'eviden',
+      title: 'Eviden Czech Republic',
+      alt: 'Eviden logo',
+    },
+    {
+      to: 'https://www.exponet.cz/',
+      image: 'exponet',
+      title: 'Exponet - Your Event Manager',
+      alt: 'Exponet logo',
+    },
+  ],
+]
