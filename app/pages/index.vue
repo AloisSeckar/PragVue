c3ec65234c752c6cfceaa1f53d7244cd73dd3e8b
Alois Sečkár <ellrohir@seznam.cz>
2025-04-14 22:59:33 +0200

    refactor: move speaker and sponsor info into separate data files

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -296,100 +296,10 @@
 </template>
 
 <script setup lang="ts">
+import { speakers } from '@/utils/speakers-2025'
+import { sponsors } from '@/utils/sponsors-2025'
+
 const tickets = 'https://pragvue.konference.cz/'
-
-const speakers = computed(() => [
-  {
-    name: 'Alex Lichter',
-    country: 'NED',
-    github: 'TheAlexLichter',
-  },
-  {
-    name: 'Jakub Andrzejewski',
-    country: 'POL',
-    github: 'Baroshem',
-  },
-  {
-    name: 'Louëlla Creemers',
-    country: 'NED',
-    github: 'LouellaCreemers',
-  },
-  {
-    name: 'Julien Huang',
-    country: 'FRA',
-    github: 'huang-julien',
-  },
-  {
-    name: 'Adam Berecz',
-    country: 'HUN',
-    github: 'adamberecz',
-  },
-  {
-    name: 'Anastasiia Zvenigorodskaia',
-    country: 'ESP',
-    github: 'zvenigorodskaia',
-  },
-  {
-    name: 'Raí Siqueira',
-    country: 'BRA',
-    github: 'raisiqueira',
-  },
-  {
-    name: 'Alvaro Saburido Rodriguez',
-    country: 'ESP',
-    github: 'alvarosabu',
-  },
-  {
-    name: 'Thorsten Seyschab',
-    country: 'GER',
-    github: 'toddeTV',
-  },
-  {
-    name: 'Konstantin Bifert',
-    country: 'NED',
-    github: 'kissu',
-  },
-  {
-    name: 'Juan Andrés Núñez',
-    country: 'ESP',
-    github: 'juanwmedia',
-  },
-  {
-    name: 'Alexander Opalic',
-    country: 'GER',
-    github: 'alexanderop',
-  },
-  {
-    name: 'Alois Sečkár',
-    country: 'CZE',
-    github: 'AloisSeckar',
-  },
-])
-
-const sponsors = computed(() => {
-  return [
-    [
-      {
-        to: 'https://www.evidencz.com/',
-        image: 'atos',
-        title: 'Atos Czech Republic',
-        alt: 'Atos logo',
-      },
-      {
-        to: 'https://www.evidencz.com/',
-        image: 'eviden',
-        title: 'Eviden Czech Republic',
-        alt: 'Eviden logo',
-      },
-      {
-        to: 'https://www.exponet.cz/',
-        image: 'exponet',
-        title: 'Exponet - Your Event Manager',
-        alt: 'Exponet logo',
-      },
-    ],
-  ]
-})
 
 const venueImages = useT('pages.home.venue_images')
 const pragueImages = useT('pages.home.prague_images')
