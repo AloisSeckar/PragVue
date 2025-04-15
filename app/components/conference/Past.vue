0c917cf3aa6db0787b3e46d6419e13be6d662493
Alois Sečkár <ellrohir@seznam.cz>
2025-04-15 19:29:06 +0200

    refactor: split index into components

---
+++ app/components/conference/Past.vue
@@ -0,0 +1,26 @@
+<template>
+  <LayoutContainer>
+    <LayoutContainerContent>
+      <UiHeading class="mb-12 text-vue">
+        {{ $t('headings.look_back_on_2024') }}
+      </UiHeading>
+
+      <i18n-t
+        tag="p"
+        scope="global"
+        keypath="pages.home.look_back_on_2024"
+        class="mb-12 text-base max-w-[128ch] text-center text-balance"
+      />
+
+      <NuxtLink to="/2024">
+        <UiImage
+          class="w-auto aspect-[1200/971] max-h-[330px] mx-auto"
+          fit="contain"
+          src="/pragvue2024.webp"
+          :title="$t('pages.home.link_2024')"
+          :alt="$t('accessibility.photo_of_the_speakers_from_2024')"
+        />
+      </NuxtLink>
+    </LayoutContainerContent>
+  </LayoutContainer>
+</template>
---
+++ app/components/conference/Prague.vue
@@ -0,0 +1,48 @@
+<template>
+  <LayoutContainer>
+    <LayoutContainerContent>
+      <UiHeading class="mb-6 text-vue">
+        {{ $t('headings.prague') }}
+      </UiHeading>
+
+      <i18n-t
+        tag="p"
+        scope="global"
+        keypath="pages.home.prague_info"
+        class="mb-12 text-base max-w-[1150px] text-left text-balance"
+      />
+
+      <div class="flex flex-row gap-2 sm:gap-6">
+        <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
+          <NuxtImg
+            class="mb-10 mx-auto w-auto max-h-[300px]"
+            src="/prague1.webp"
+            :title="pragueImages"
+            :alt="pragueImages"
+          />
+        </NuxtLink>
+        <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
+          <NuxtImg
+            class="mb-10 mx-auto w-auto max-h-[300px]"
+            src="/prague2.webp"
+            :title="pragueImages"
+            :alt="pragueImages"
+          />
+        </NuxtLink>
+        <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
+          <NuxtImg
+            class="mb-10 mx-auto w-auto max-h-[300px]"
+            fit="contain"
+            src="/prague3.webp"
+            :title="pragueImages"
+            :alt="pragueImages"
+          />
+        </NuxtLink>
+      </div>
+    </LayoutContainerContent>
+  </LayoutContainer>
+</template>
+
+<script setup lang="ts">
+const pragueImages = useT('pages.home.prague_images')
+</script>
