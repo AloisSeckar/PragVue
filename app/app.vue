45738e992154bdb25c676588ca4dd02738e8bf4f
AloisSeckar <ellrohir@seznam.cz>
2025-06-19 10:13:01 +0200

    refactor: move footer to own component

--- app/app.vue
+++ app/app.vue
@@ -1,33 +1,6 @@
 <template>
   <NuxtPage />
-  <LayoutContainer tag="footer">
-    <LayoutContainerContent size-y="small">
-      <div class="mb-2 font-mono font-semibold text-center text-woodsmoke-300 text-lg before:content-['<!--_'] after:content-['_-->']">
-        © <UiPragVue :year />
-      </div>
-      <div class="mb-2 font-mono text-sm text-center text-woodsmoke-300">
-        Contact us: <NuxtLink to="mailto:info@pragvue.com" class=" hover:text-vue-light">info [at] pragvue.com</NuxtLink>
-      </div>
-      <div class="flex flex-row gap-3 items-center justify-center mb-2 font-mono text-sm text-center text-woodsmoke-300">
-        Follow us:
-        <NuxtLink to="https://bsky.app/profile/pragvue.com">
-          <img src="/bluesky.png" title="Follow us on Bluesky" alt="PragVue Bluesky" class="p-1 h-6 rounded-md ring-2 ring-vue hover:ring-vue-light">
-        </NuxtLink>
-        <NuxtLink to="https://x.com/PragVue_conf">
-          <img src="/twitter.png" title="Follow us on Twitter/X" alt="PragVue Twitter/X" class="p-1 h-6 rounded-md ring-2 ring-vue hover:ring-vue-light">
-        </NuxtLink>
-        <NuxtLink to="https://www.linkedin.com/company/pragvue/">
-          <img src="/linkedin.png" title="Follow us on LinkedIn" alt="PragVue LinkedIn" class="p-1 h-6 rounded-md ring-2 ring-vue hover:ring-vue-light">
-        </NuxtLink>
-      </div>
-      <div class="text-xs text-center">
-        Powered by
-        <NuxtLink to="https://github.com/AloisSeckar/nuxt-ignis" class="text-vue hover:text-vue-light">
-          nuxt-ignis
-        </NuxtLink>
-      </div>
-    </LayoutContainerContent>
-  </LayoutContainer>
+  <TheFooter />
 </template>
 
 <script setup lang="ts">
@@ -51,8 +24,6 @@ useSeoMeta({
   ogImage: 'https://pragvue.com/pragvue.jpg',
   twitterCard: 'summary_large_image',
 })
-
-const year = useState(() => new Date().getFullYear())
 </script>
 
 <style>
---
+++ app/components/TheFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <LayoutContainer tag="footer">
+    <LayoutContainerContent size-y="small">
+      <div class="mb-2 font-mono font-semibold text-center text-woodsmoke-300 text-lg before:content-['<!--_'] after:content-['_-->']">
+        © <UiPragVue :year="new Date().getFullYear()" />
+      </div>
+      <div class="mb-2 font-mono text-sm text-center text-woodsmoke-300">
+        Contact us: <NuxtLink to="mailto:info@pragvue.com" class=" hover:text-vue-light">info [at] pragvue.com</NuxtLink>
+      </div>
+      <div class="flex flex-row gap-3 items-center justify-center mb-2 font-mono text-sm text-center text-woodsmoke-300">
+        Follow us:
+        <NuxtLink to="https://bsky.app/profile/pragvue.com">
+          <img src="/bluesky.png" title="Follow us on Bluesky" alt="PragVue Bluesky" class="p-1 h-6 rounded-md ring-2 ring-vue hover:ring-vue-light">
+        </NuxtLink>
+        <NuxtLink to="https://x.com/PragVue_conf">
+          <img src="/twitter.png" title="Follow us on Twitter/X" alt="PragVue Twitter/X" class="p-1 h-6 rounded-md ring-2 ring-vue hover:ring-vue-light">
+        </NuxtLink>
+        <NuxtLink to="https://www.linkedin.com/company/pragvue/">
+          <img src="/linkedin.png" title="Follow us on LinkedIn" alt="PragVue LinkedIn" class="p-1 h-6 rounded-md ring-2 ring-vue hover:ring-vue-light">
+        </NuxtLink>
+      </div>
+      <div class="text-xs text-center">
+        Powered by
+        <NuxtLink to="https://github.com/AloisSeckar/nuxt-ignis" class="text-vue hover:text-vue-light">
+          nuxt-ignis
+        </NuxtLink>
+      </div>
+    </LayoutContainerContent>
+  </LayoutContainer>
+</template>
