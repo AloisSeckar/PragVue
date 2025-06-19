c8724704eb14d892cc9810101a1b613b303947fd
AloisSeckar <ellrohir@seznam.cz>
2025-06-19 11:26:18 +0200

    refactor: schedule data to separate data file

---
+++ app/utils/schedule-2025.ts
@@ -0,0 +1,83 @@
+export const schedule = [
+  {
+    time: '08:30',
+    title: 'Doors open',
+  },
+  {
+    time: '09:00',
+    title: 'Keynote: Modern JavaScript Tooling',
+    speaker: 'Alexander Lichter',
+  },
+  {
+    time: '09:30',
+    title: 'Dungeons & Developers - Patterns for Great DX',
+    speaker: 'Jakub Andrzejewski',
+  },
+  {
+    time: '10:00',
+    title: 'From Object to Interface—Serialized Forms with Vueform',
+    speaker: 'Adam Berecz',
+  },
+  {
+    time: '10:30',
+    title: 'Coffee Break ☕🥐',
+  },
+  {
+    time: '11:00',
+    title: 'Overcoming Third-party Script Headaches in Vue with a Single Library',
+    speaker: 'Julien Huang',
+  },
+  {
+    time: '11:30',
+    title: 'How to Nuxt Modules',
+    speaker: 'Thorsten Seyschab',
+  },
+  {
+    time: '12:00',
+    title: 'Ignite your Nuxt development with Layers',
+    speaker: 'Alois Sečkár',
+  },
+  {
+    time: '12:30',
+    title: 'Lunch Break 🍲🧆',
+  },
+  {
+    time: '13:30',
+    title: 'What the new European Accessibility Act Means for Web Developers',
+    speaker: 'Louëlla Creemers',
+  },
+  {
+    time: '14:00',
+    title: 'Revamping Your Vue.js Stack: Lessons from a Real Migration',
+    speaker: 'Raí Siqueira',
+  },
+  {
+    time: '14:30',
+    title: 'Scaling Vue in Enterprise SaaS: Lessons from the Trenches',
+    speaker: 'Juan Andrés Núñez',
+  },
+  {
+    time: '15:00',
+    title: 'Coffee Break ☕🥐',
+  },
+  {
+    time: '15:30',
+    title: 'How to Structure Vue Projects',
+    speaker: 'Alexander Opalic',
+  },
+  {
+    time: '16:00',
+    title: 'How to Make Open Source Profitable',
+    speaker: 'Anastasiia Zvenigorodskaia',
+  },
+  {
+    time: '16:30',
+    title: 'The Land of the VueJS.land',
+    speaker: 'Konstantin Bifert',
+  },
+  {
+    time: '17:00',
+    title: 'What to Expect in the v5 of TresJS',
+    speaker: 'Alvaro Saburido Rodriguez',
+  },
+]
