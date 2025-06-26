3f887745a84103ab43c9679a3b7c316745aca967
AloisSeckar <ellrohir@seznam.cz>
2025-06-26 11:56:09 +0200

    refactor: 2024 speaker data to data file

---
+++ app/utils/speakers-2024.ts
@@ -0,0 +1,42 @@
+export const speakers = [
+  {
+    name: 'Daniel Roe',
+    img: 'roe_d',
+    dscr: 'gave us insight about upcomming Nuxt 4',
+  },
+  {
+    name: 'Estéban Soubiran',
+    img: 'soubrain_e',
+    dscr: 'demonstrated fullstack application with NuxtHub',
+  },
+  {
+    name: 'Alexander Lichter',
+    img: 'lichter_a',
+    dscr: 'shared best practices for working with Vue composables',
+  },
+  {
+    name: 'Ramona Schwering',
+    img: 'schwering_r',
+    dscr: 'had a talk about Automated Tests in the World of Web Security',
+  },
+  {
+    name: 'Thorsten Seyschab',
+    img: 'seyschab_t',
+    dscr: 'showed us how to move Vue to 3D',
+  },
+  {
+    name: 'Alois Sečkár',
+    img: 'seckar_a',
+    dscr: 'presented his experience about implementing big Nuxt project',
+  },
+  {
+    name: 'Konstantin Bifert',
+    img: 'bifert_k',
+    dscr: 'taught us how to ship Vue.js on different platforms',
+  },
+  {
+    name: 'Juan Andrés Núñez',
+    img: 'nunez_j',
+    dscr: 'leveraged Firebase for Real-Time Vue app updates',
+  },
+]
