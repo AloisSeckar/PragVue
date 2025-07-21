0e927d70e67cafb5e792d3f0d263a2cbbbc12d28
AloisSeckar <ellrohir@seznam.cz>
2025-07-21 09:55:58 +0200

    fix: defer loading of Termly script

--- app/app.vue
+++ app/app.vue
@@ -11,7 +11,7 @@ useHead({
   bodyAttrs: {
     class: 'bg-woodsmoke-900',
   },
-  script: [{ src: 'https://app.termly.io/resource-blocker/bbc9f62b-96e1-4064-bea5-e604bb669e55?autoBlock=on' }],
+  script: [{ src: 'https://app.termly.io/resource-blocker/bbc9f62b-96e1-4064-bea5-e604bb669e55?autoBlock=on', defer: true }],
 })
 
 useSeoMeta({
