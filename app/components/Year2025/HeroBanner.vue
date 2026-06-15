39f796f8190f94989d1f74b59f9ea5da0faf287a
Alois Sečkár <ellrohir@seznam.cz>
2026-06-15 14:27:50 +0200

    refactor: year 2025 to its own scope

--- app/components/Year2025/HeroBanner.vue
+++ app/components/Year2025/HeroBanner.vue
@@ -55,20 +55,22 @@
     </UiSwiper>
 
     <LayoutContainerContent class="flex flex-col items-center justify-start sm:justify-center gap-4 sm:gap-6">
-      <TheLogo tag="h1" />
+      <h1 class="font-default font-bold pt-12 px-4 sm:pt-0 whitespace-nowrap text-woodsmoke-600 before:content-['<_'] after:content-['_/>'] pgv-logo">
+        <UiPragVue :year="2025" />
+      </h1>
 
       <p class="mb-6 text-xl md:text-2xl text-center">
         {{ $t('headings.the_czech_vuejs_conference') }}
       </p>
 
       <p class="mb-1 text-xl md:text-3xl text-center font-bold">
-        {{ $t('2025.when') }}
+        {{ $t('year.2025.when') }}
       </p>
       <p class="mb-4 text-xl md:text-2xl text-center">
         <NuxtLink to="https://www.hotel-grandium.cz/" class=" hover:text-vue-light">
-          {{ $t('2025.where') }}
+          {{ $t('year.2025.where') }}
         </NuxtLink>
-        &nbsp;<a href="https://maps.app.goo.gl/vWW31EiriqbdCpTW8" :title="$t('2025.map')">🗺️</a>
+        &nbsp;<a href="https://maps.app.goo.gl/vWW31EiriqbdCpTW8" :title="$t('year.2025.map')">🗺️</a>
       </p>
 
       <div class="flex flex-col sm:flex-row gap-4 items-center">
@@ -101,5 +103,9 @@ const otherSponsors = sponsors.filter(s => s.tier > 1)
 .pgv-hero-banner__bg {
   transform: translateX(-50%);
   width: clamp(1200px, 100%, 2000px);
+}
+
+.pgv-logo {
+    font-size: clamp(2rem, 10vw, 6rem);
 }
 </style>
