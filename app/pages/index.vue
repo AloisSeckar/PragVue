cf99e6f55a2d07e624f3e908aa3843881bfb6335
AloisSeckar <alois.seckar@gmail.com>
2026-06-26 16:25:46 +0200

    refactor: page header to its own component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
-    <header class="pt-2 h-lvh relative isolation overflow-hidden flex flex-col items-center justify-center">
-      <!-- <ConferenceSponsorsHeader /> -->
-      <div class="font-bold mb-3">
-        <UiShimmer>
-          <h1 style="font-size: clamp(40px, 12.5vw, 80px)">
-            <UiPragVue2026 :year="2026" />
-          </h1>
-        </UiShimmer>
-      </div>
-      <main class="text-center">
-        <div class="text-2xl mb-2">
-          <span class="text-gray-400">{{ $t('2026.subtitle') }}</span>
-        </div>
-        <div class="text-3xl mb-6">
-          <span class="text-yellow-400">{{ $t('2026.date') }}</span>
-        </div>
-        <UiMoreInfo anchor="introduction" />
-      </main>
-    </header>
+    <ConferenceHeader />
     <ConferenceIntroduction />
     <LazyConferenceInfo2026 />
     <LazyConferenceNewsletter />
