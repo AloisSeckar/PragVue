ac7b709a984bb7dcc2f62997c221192d4860e24e
AloisSeckar <alois.seckar@gmail.com>
2026-06-29 13:28:09 +0200

    feat: style venue and prague components better

--- app/components/conference/Prague.vue
+++ app/components/conference/Prague.vue
@@ -1,38 +1,40 @@
 <template>
   <LayoutContainer>
-    <UiHeading class="mb-6 text-vue-light">
-      {{ $t('headings.prague') }}
-    </UiHeading>
+    <div class="flex flex-col items-start">
+      <UiHeading class="mb-6 text-vue-light">
+        {{ $t('headings.prague') }}
+      </UiHeading>
 
-    <i18n-t
-      tag="p"
-      scope="global"
-      keypath="pages.home.prague_info"
-      class="mb-8 tracking-[0.01em] max-w-285 text-left text-balance"
-    />
+      <i18n-t
+        tag="p"
+        scope="global"
+        keypath="pages.home.prague_info"
+        class="mb-8 tracking-[0.01em] max-w-285 text-left text-balance"
+      />
 
-    <div class="flex flex-col lg:flex-row gap-4 mb-10 px-2">
-      <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
-        <NuxtImg
-          class="mx-auto w-auto max-h-80"
-          src="/prague1.webp" loading="lazy"
-          :title="pragueImages" :alt="pragueImages"
-        />
-      </NuxtLink>
-      <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
-        <NuxtImg
-          class="mx-auto w-auto max-h-80"
-          src="/prague2.webp" loading="lazy"
-          :title="pragueImages" :alt="pragueImages"
-        />
-      </NuxtLink>
-      <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
-        <NuxtImg
-          class="mx-auto w-auto max-h-80"
-          src="/prague3.webp" loading="lazy"
-          :title="pragueImages" :alt="pragueImages"
-        />
-      </NuxtLink>
+      <div class="flex flex-col md:flex-row gap-4 mb-10 px-2">
+        <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
+          <NuxtImg
+            class="mx-auto max-h-80 w-full object-cover"
+            src="/prague1.webp" loading="lazy"
+            :title="pragueImages" :alt="pragueImages"
+          />
+        </NuxtLink>
+        <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
+          <NuxtImg
+            class="mx-auto max-h-80 w-full object-cover"
+            src="/prague2.webp" loading="lazy"
+            :title="pragueImages" :alt="pragueImages"
+          />
+        </NuxtLink>
+        <NuxtLink to="https://prague.eu/en/" class="cursor-pointer">
+          <NuxtImg
+            class="mx-auto max-h-80 w-full object-cover"
+            src="/prague3.webp" loading="lazy"
+            :title="pragueImages" :alt="pragueImages"
+          />
+        </NuxtLink>
+      </div>
     </div>
   </LayoutContainer>
 </template>
--- app/components/conference/Venue.vue
+++ app/components/conference/Venue.vue
@@ -1,44 +1,46 @@
 <template>
   <LayoutContainer>
-    <UiHeading class="mb-6 text-vue">
-      {{ $t('headings.venue') }}
-    </UiHeading>
+    <div class="flex flex-col items-end">
+      <UiHeading class="mb-6 text-vue text-right">
+        {{ $t('headings.venue') }}
+      </UiHeading>
 
-    <i18n-t
-      tag="p"
-      scope="global"
-      keypath="venue.venue_info"
-      class="mb-8 tracking-[0.01em] max-w-285 text-left text-balance"
-    >
-      <template #venue>
-        <NuxtLink to="https://arthurspub.cz/en/" class="text-vue hover:text-vue-light font-bold hover:cursor-pointer">
-          Arthur's Pub
-        </NuxtLink><span style="margin-left: -5px">,</span>
-      </template>
-    </i18n-t>
+      <i18n-t
+        tag="p"
+        scope="global"
+        keypath="venue.venue_info"
+        class="mb-8 tracking-[0.01em] max-w-285 text-right text-balance"
+      >
+        <template #venue>
+          <NuxtLink to="https://arthurspub.cz/en/" class="text-vue hover:text-vue-light font-bold hover:cursor-pointer">
+            Arthur's Pub
+          </NuxtLink><span style="margin-left: -5px">,</span>
+        </template>
+      </i18n-t>
 
-    <div class="flex flex-col lg:flex-row gap-4 mb-10 px-2">
-      <NuxtLink to="https://arthurspub.cz/en/corporate-events" class="cursor-pointer">
-        <NuxtImg
-          class="mx-auto w-auto max-h-80"
-          src="/arthurs-pub-1.webp" loading="lazy"
-          :title="venueImages" :alt="venueImages"
-        />
-      </NuxtLink>
-      <NuxtLink to="https://arthurspub.cz/en/corporate-events" class="cursor-pointer">
-        <NuxtImg
-          class="mx-auto w-auto max-h-80"
-          src="/arthurs-pub-2.webp" loading="lazy"
-          :title="venueImages" :alt="venueImages"
-        />
-      </NuxtLink>
-      <NuxtLink to="https://arthurspub.cz/en/corporate-events" class="cursor-pointer">
-        <NuxtImg
-          class="mx-auto w-auto max-h-80"
-          src="/arthurs-pub-3.webp" loading="lazy"
-          :title="venueImages" :alt="venueImages"
-        />
-      </NuxtLink>
+      <div class="flex flex-col md:flex-row gap-4 mb-10 px-2">
+        <NuxtLink to="https://arthurspub.cz/en/corporate-events" class="cursor-pointer">
+          <NuxtImg
+            class="mx-auto max-h-80 w-full object-cover"
+            src="/arthurs-pub-1.webp" loading="lazy"
+            :title="venueImages" :alt="venueImages"
+          />
+        </NuxtLink>
+        <NuxtLink to="https://arthurspub.cz/en/corporate-events" class="cursor-pointer">
+          <NuxtImg
+            class="mx-auto max-h-80 w-full object-cover"
+            src="/arthurs-pub-2.webp" loading="lazy"
+            :title="venueImages" :alt="venueImages"
+          />
+        </NuxtLink>
+        <NuxtLink to="https://arthurspub.cz/en/corporate-events" class="cursor-pointer">
+          <NuxtImg
+            class="mx-auto max-h-80 w-full object-cover"
+            src="/arthurs-pub-3.webp" loading="lazy"
+            :title="venueImages" :alt="venueImages"
+          />
+        </NuxtLink>
+      </div>
     </div>
   </LayoutContainer>
 </template>
